9cdc2ec210807445933bc79f2e760ce0dc8a0152
域外創音 <kaitensekai@qq.com>
2021-09-27 16:08:21 +0800

    update `xmake.lua`: build with xmake swig support

--- xmake.lua
+++ xmake.lua
@@ -8,31 +8,33 @@ add_requires("raylib 3.7.0")
 add_requires("luajit 2.1.0-beta3", {optional=true})
 
 target("swigraylib_lua")
-    set_kind("shared")
+
     before_build(function(target)
-        local raylibDefs = {"-D_DEFAULT_SOURCE -DPLATFORM_DESKTOP -DGRAPHICS_API_OPENGL_33 -DPHYSAC_IMPLEMENTATION"}
-        if is_plat("windows") then
-            raylibDefs[#raylibDefs+1] = "-D_WIN32"
-        elseif is_plat("linux") then
-            raylibDefs[#raylibDefs+1] = "-D__linux__"
-        elseif is_plat("macosx") then
-            raylibDefs[#raylibDefs+1] = "-D__APPLE__"
-        else
+        if not (is_plat("windows") or is_plat("linux") or is_plat("macosx")) then
             os.raise("This xmake script doesn't support this target platform at the moment...")
         end
-        local swigParams = {"-lua", "-no-old-metatable-bindings"}
-        for i = 1, #raylibDefs do swigParams[#swigParams+1] = raylibDefs[i] end
-        swigParams[#swigParams+1] = "raylib.i"
-        os.runv("swig", swigParams)
-        cprint("${yellow underline}SwigCmd: swig "..table.concat(swigParams, ' ').." ${clear}")
-        target:add("cxflags", raylibDefs)
     end)
-    if is_plat("linux") then
-        add_cxflags("-fPIC")
-    end
+
+    set_kind("shared")
+    add_rules("swig.c", {moduletype = "lua"})
     add_packages("raylib")
-    add_files("raylib_wrap.c")
 
     -- Choose one of the Lua package:
     -- add_packages("lua")
     add_packages("luajit")
+
+    local raylibDefs = {"-D_DEFAULT_SOURCE", "-DPLATFORM_DESKTOP", "-DGRAPHICS_API_OPENGL_33", "-DPHYSAC_IMPLEMENTATION"}
+    if is_plat("windows") then
+        raylibDefs[#raylibDefs+1] = "-D_WIN32"
+    elseif is_plat("linux") then
+        raylibDefs[#raylibDefs+1] = "-D__linux__"
+        add_cxflags("-fPIC")
+    elseif is_plat("macosx") then
+        raylibDefs[#raylibDefs+1] = "-D__APPLE__"
+    end
+
+    local swigParams = {"-no-old-metatable-bindings"}
+    table.join2(swigParams, raylibDefs)
+
+    add_files("raylib.i", {swigflags = swigParams})
+    add_cxflags(table.unpack(raylibDefs))
